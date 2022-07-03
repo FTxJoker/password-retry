fceefc33f159671e18eaa46a3b424713f257bdb4
stay0913 <stay0913@bear-service.net>
2022-07-03 15:00:31 +0800

    Update

---
+++ password.py
@@ -0,0 +1,10 @@
+password = 'a123456'
+y = 3
+while y > 0:
+    pwd = input('請輸入密碼: ')
+    if pwd == password:
+        print('登入成功!')
+        break
+    else:
+        y = y - 1
+        print('密碼錯誤! 還有', y, '次機會')
